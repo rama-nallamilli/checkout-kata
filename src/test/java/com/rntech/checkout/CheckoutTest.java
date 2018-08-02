4310e328af5973ab5849181ece9ecbef0b7417d2
ramanallamilli <rama.nallamilli@gmail.com>
2018-08-02 20:30:09 +0100

    Add additional test cases for basket.

--- src/test/java/com/rntech/checkout/CheckoutTest.java
+++ src/test/java/com/rntech/checkout/CheckoutTest.java
@@ -6,19 +6,48 @@ import com.rntech.checkout.pricing.domain.ItemPrice;
 import com.rntech.checkout.pricing.rules.GroupDiscountOffers;
 import com.rntech.checkout.sku.ItemSku;
 import com.rntech.checkout.sku.SkuNotFoundException;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import java.util.*;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class CheckoutTest {
     private final PricingRule noDiscountPricingRule = (unitPriceInPence, numOrdered) -> unitPriceInPence * numOrdered;
 
     @Test
-    void checkout_shouldCalculateTotalPriceForMultipleItems() {
+    void checkout_shouldCalculateTotalPriceForSingleItem() {
+        Checkout checkout = new Checkout(noDiscountPricingRule);
+
+        Map<ItemSku, ItemPrice> rules = new HashMap<>();
+        rules.put(new ItemSku("A"), new ItemPrice(100));
+
+        Basket basket = createBasket("A");
+
+        int totalPrice = checkout.checkout(basket, rules);
+        assertEquals(100, totalPrice);
+    }
+
+    @Test
+    void checkout_shouldCalculateTotalPriceForItemsWithDiscount() {
+        Checkout checkout = new Checkout(noDiscountPricingRule);
+
+        Map<ItemSku, ItemPrice> rules = new HashMap<>();
+        rules.put(new ItemSku("D"), new ItemPrice(40, GroupDiscountOffers.twoFor45));
+
+        Basket basket = createBasket("D", "D");
+
+        int totalPrice = checkout.checkout(basket, rules);
+        assertEquals(45, totalPrice);
+    }
+
+    @Test
+    void checkout_shouldCalculateTotalPriceForMixedItems() {
         Checkout checkout = new Checkout(noDiscountPricingRule);
 
         Map<ItemSku, ItemPrice> rules = new HashMap<>();
@@ -27,13 +56,10 @@ class CheckoutTest {
         rules.put(new ItemSku("C"), new ItemPrice(20));
         rules.put(new ItemSku("D"), new ItemPrice(15));
 
-        List<ItemSku> basket = Arrays
-                .stream(new String[]{"A", "A", "A", "B", "B", "C", "D"})
-                .map(ItemSku::new)
-                .collect(Collectors.toList());
+        Basket basket = createBasket("A", "A", "A", "B", "B", "C", "D", "A");
 
-        int totalPrice = checkout.checkout(new Basket(basket), rules);
-        assertEquals(210, totalPrice);
+        int totalPrice = checkout.checkout(basket, rules);
+        assertEquals(260, totalPrice);
     }
 
     @Test
@@ -43,9 +69,9 @@ class CheckoutTest {
         Map<ItemSku, ItemPrice> rules = new HashMap<>();
         rules.put(new ItemSku("A"), new ItemPrice(50, GroupDiscountOffers.threeFor130));
 
-        List<ItemSku> basket = Collections.emptyList();
+        Basket basket = createBasket();
 
-        int totalPrice = checkout.checkout(new Basket(basket), rules);
+        int totalPrice = checkout.checkout(basket, rules);
         assertEquals(0, totalPrice);
     }
 
@@ -57,14 +83,19 @@ class CheckoutTest {
         rules.put(new ItemSku("A"), new ItemPrice(50, GroupDiscountOffers.threeFor130));
         rules.put(new ItemSku("B"), new ItemPrice(30, GroupDiscountOffers.twoFor45));
 
-        List<ItemSku> basket = Arrays
-                .stream(new String[]{"A", "B", "Z"})
+        Basket basket = createBasket("A", "B", "Z");
+
+        Throwable ex = assertThrows(SkuNotFoundException.class, () ->
+                checkout.checkout(basket, rules));
+
+        assertEquals("Could not find SKU for Z", ex.getMessage());
+    }
+
+    private Basket createBasket(String...skus) {
+        List<ItemSku> items = Arrays.stream(skus)
                 .map(ItemSku::new)
                 .collect(Collectors.toList());
 
-        Throwable ex = assertThrows(SkuNotFoundException.class, () ->
-                checkout.checkout(new Basket(basket), rules));
-
-        assertEquals("Could not find SKU for Z", ex.getMessage());
+        return new Basket(items);
     }
 }
